cc1d3b483a0789b33547d3d33cc3fc52de1f6ab0
samussouza <samuel.ssouza@sptech.school>
2024-06-14 00:38:03 -0300

    adicionado alertas visual na dash

--- SIte Institucional/public/js/alerta2.js
+++ SIte Institucional/public/js/alerta2.js
@@ -1,9 +1,10 @@
 //variaveis que armazenam as valores atuais das medidas de temp e lumin
-id_habitat = sessionStorage.ID_HABITAT_ALERTA
-dataHora_alerta = sessionStorage.DADO_ULTIMO_ALERTA
+var id_habitat = sessionStorage.ID_HABITAT_ALERTA
+var dataHora_alerta = sessionStorage.DADO_ULTIMO_ALERTA
 
 
 function validarAlertas() {
+    mudarKPIMedias()
     console.log('Iniciando validação para emitir os alertas')
     var fk_empresa1 = sessionStorage.FK_EMPRESA;
     var habitatID = sessionStorage.FK_HABITAT;
@@ -32,12 +33,35 @@ function validarAlertas() {
                 sessionStorage.MEDIA_ATUAL_LUMIN = AtualLumin;
                 console.log(AtualLumin, AtualTemp);
                 avaliarMetrica()
+                mudarKPIMedias()
             }
         }).catch(function (erro) {
             console.error(`#ERRO: ${erro.message}`);
         });
 }
+function mudarKPIMedias() {
+    var mediaTemp = parseFloat(sessionStorage.getItem('MEDIA_GERAL_TEMP'));
+    var mediaLumin = parseFloat(sessionStorage.getItem('MEDIA_GERAL_LUMIN'));
 
+    const spanTemp = document.querySelector('.divTextoKPI3 span');
+    const spanLumin = document.querySelector('.divTextoKPI4 span');
+    // var span_temp = document.querySelectorAll('media-temp');
+    // var span_media_lumi = document.getElementById('divTextoKPI');
+
+    if (mediaTemp < 22 || mediaTemp > 29) {
+        spanTemp.style.color = 'red';
+        console.log('Temperatura fora do intervalo');
+     
+    }else{
+        spanTemp.style.color = '#548D3D';
+    }
+     if (mediaLumin < 400 || mediaLumin > 800) {
+        spanLumin.style.color = 'red';
+        console.log('Luminosidade fora do intervalo');
+    } else {
+        spanLumin.style.color = '#548D3D';
+    }
+}
 function avaliarMetrica() {
     const temp = sessionStorage.MEDIA_ATUAL_TEMP
     const lumin = sessionStorage.MEDIA_ATUAL_LUMIN
@@ -46,26 +70,36 @@ function avaliarMetrica() {
     const acimaTemp = 29;
     const acimaLumin = 800;
     const abaixoLumin = 400;
-
+    mudarCorKPI()
     if (temp <= abaixoTemp || temp >= acimaTemp) {
-    //    const habitatLista = document.querySelector('alistaHabitats')
-    //    habitatLista.style.color = 'blue'
+        const habitatLista = document.getElementById('nomeHabitat')
+        habitatLista.style.color = 'red'
         document.getElementById('habitatAlerta').innerHTML = `
                 Alerta no Habitat ${id_habitat} <br>
                 Temperatura fora da média
                 `
         mostrarAlerta();
+        mudarCorKPI()
+
     }
 
     else if (lumin > acimaLumin || lumin < abaixoLumin) {
-    //     const habitatLista = document.querySelector('alistaHabitats')
-    //    habitatLista.style.color = 'blue'
+        const habitatLista = document.getElementById('nomeHabitat')
+        habitatLista.style.color = 'red'
         document.getElementById('habitatAlerta').innerHTML = `
             Alerta no Habitat ${id_habitat} <br>
             Luminosidade fora da média
             `
         mostrarAlerta();
+        mudarCorKPI()
     }
+
+    else {
+        const habitatLista = document.getElementById('nomeHabitat')
+        habitatLista.style.color = '#548D3D'
+
+    }
+
 
 }
 
@@ -84,4 +118,17 @@ function mostrarAlerta() {
 
 }
 
-
+function mudarCorKPI() {
+    const temp = sessionStorage.MEDIA_ATUAL_TEMP;
+    const lumin = sessionStorage.MEDIA_ATUAL_LUMIN;
+    const kpiTemp = document.getElementById('medidasAtuaisTempLumin');
+    const kpiLumin = document.getElementById('medidasAtuaisTempLumin2');
+    if (temp < 22 || temp > 29) {
+        kpiTemp.style.color = 'red'
+    } else if (lumin < 400 || lumin > 800) {
+        kpiLumin.style.color = 'red'
+    } else {
+        kpiTemp.style.color = '#548D3D'
+        kpiLumin.style.color = '#548D3D'
+    }
+}
